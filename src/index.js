64ffeb6f0de2858c2c5e4399cbe52f2836e44e26
Eugeniy Bekanurovich <passivevoice3@gmail.com>
2020-10-18 19:15:02 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 === 1) return false;	
+
+  let arr = str.split(''),
+   bracketArr = bracketsConfig,
+   result = [];
+
+  for(let i = 0; i < arr.length; i++) {
+   for(let j = 0; j < bracketArr.length; j++) {
+     if (result[result.length - 1] === bracketArr[j][0] && arr[i] === bracketArr[j][1]) {
+       result.pop();
+      } else if (arr[i] === bracketArr[j][0]) {
+       result.push(arr[i]);
+      }
+    }
+  }
+  
+  return result.length === 0
 }
